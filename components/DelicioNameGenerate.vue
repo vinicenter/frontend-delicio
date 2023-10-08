273f64d53ecfed9a41f0a83f90ccfd00762d209f
vinicenter <viniemidiobosi@gmail.com>
2023-10-08 17:00:01 -0300

    feat(generate): create generate component app

---
+++ components/DelicioNameGenerate.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { data, refresh } = await useFetch('/api/delicio')
+</script>
+
+<template>
+  <div>
+    <img src="/delicio.jpeg" />
+    {{ data?.name }}
+    <div @click="refresh()">Refresh</div>
+  </div>
+</template>
